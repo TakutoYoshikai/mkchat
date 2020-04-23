bf56b15a7f7c80e259373aa94160022c0c33a895
takuto <takuto.yoshikai@gmail.com>
2020-04-23 21:00:39 +0900

    add export script

---
+++ export.js
@@ -0,0 +1,82 @@
+const myIcon = "images/takuto-meshi.png";
+const yourIcon = "images/hota.jpg";
+
+function myMessage(text) {
+  return `<div class="my-message">
+    <div class="message-text">${text}</div>
+    <img src="${myIcon}" class="message-icon">
+  </div>`
+}
+
+function yourMessage(text) {
+  return `<div class="message">
+    <img src="${yourIcon}" class="message-icon">
+    <div class="message-text">${text}</div>
+  </div>`
+}
+
+function yourImage(src) {
+  return `<div class="message">
+    <img src="${yourIcon}" class="message-icon">
+    <div class="message-text">
+      <img class="message-image" src="${src}">
+    </div>
+  </div>`
+}
+
+function myImage(src) {
+  return `<div class="my-message">
+    <div class="message-text">
+      <img class="message-image" src="${src}">
+    </div>
+    <img src="${myIcon}" class="message-icon">
+  </div>`
+}
+function title(text) {
+  return `<div class="center">
+    <p class="title">
+      ${text}
+    </p>
+  </div>`
+}
+
+function chat(messages) {
+  return messages.reduce(function(a, b) {
+    if (b.type === "m") {
+      return a + myMessage(b.text);
+    } else if (b.type === "y") {
+      return a + yourMessage(b.text);
+    } else if (b.type === "t") {
+      return a + title(b.text);
+    } else if (b.type === "i") {
+      return a + myImage(b.text);
+    } else if (b.type === "j") {
+      return a + yourImage(b.text);
+    }
+  }, "")
+}
+
+function csvToChat(csv) {
+  return csv.split("\n").map(function(msg) {
+    if (msg === "") {
+      return null;
+    }
+    return {
+      type: msg[0],
+      text: msg.slice(1)
+    }
+  }).filter(function(msg) {
+    return msg !== null;
+  })
+}
+
+function getChatTexts() {
+  const fs = require("fs")
+  return new Promise(function(resolve, reject) {
+    resolve(fs.readFileSync("chat.csv", "utf-8"))
+  })
+}
+
+getChatTexts().then(function(text) {
+  console.log(chat(csvToChat(text)))
+})
